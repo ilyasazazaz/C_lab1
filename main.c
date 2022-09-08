71714f5ccd7cb4b641d3a3fe6dd086ac144958f6
ilyasazazaz <113016842+ilyasazazaz@users.noreply.github.com>
2022-09-08 13:16:50 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS		//для корректной работы scanf в visual studio
+#include<stdio.h>
+
+int main()
+{
+	float a;
+	char b;
+	printf("Your float is: ");
+	scanf("%f", &a);
+	printf("Your char is: ");
+	scanf("\n%c", &b);
+
+	printf("\nYour answers is: \n%f\n%c\n", a,b);
+	return 0;
+}
